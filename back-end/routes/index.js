35a42775453e9cf3647c525ed055095b8c87c0d3
Robert Bunch <rob@digitalcrafts.com>
2017-12-07 12:00:46 -0500

    Add addStudent post and form

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,28 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require('../config/config');
+var connection = mysql.createConnection(config)
+connection.connect();
 
 /* GET home page. */
 router.get('/getStudents', function(req, res, next) {
-	const students = [
-		"Eddie",
-		"Valerie",
-		"Michael",
-		"Scott",
-		"Jennifer",
-		"Nikolas",
-		"Zach",
-		"Casey",
-		"Jamie",
-		"Amir",
-		"Akil",
-		"Binh",
-		"Jong",
-		"Jason",
-		"Chris",
-		"Saif",
-		"Taylor"
-	];
-	res.json(students);
+	const selectQuery = `SELECT * FROM students`;
+	connection.query(selectQuery,(error, results)=>{
+		if (error){
+			throw error;
+		}else{
+			res.json(results);		
+		}
+	})	
 });
+
+router.post('/addStudent',(req, res)=>{
+	res.json(req.body);
+})
+
 
 module.exports = router;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,24 +4,58 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      students: []
+    }
+  }
+
 
   componentDidMount(){
     axios.get('http://localhost:3000/getStudents')
       .then((response)=>{
         console.log(response)
+        this.setState({
+          students: response.data
+        })
       });
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+    const studentName = document.getElementById('new-student').value;
+    // console.log(event.target);
+    // We want to send studentName to the server.
+    // We CANNOT use get. We need to use post.
+    // There is an axios.post and there is axios({}).
+    // Axios always returns a proimse, so we will send data 
+    // to the express server, and wait for a JSON response.
+    // When we get it, we will move forward
+    axios({
+      method: "POST",
+      url: "http://localhost:3000/addStudent",
+      data: {
+        studentName: studentName
+      }
+    }).then((data)=>{
+      console.log(data);
+    })
+  }
+
   render() {
+    var studentsArray = this.state.students.map((student,i)=>{
+      return (<li key={i}>{student.name}</li>)
+    })
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" id="new-student" placeholder="New Student name" />
+          <button type="submit">Add Student</button>
+        </form>
+        <ul>
+          {studentsArray}
+        </ul>
       </div>
     );
   }
